f4e8b1b8a49aaebf2b82022a70904a95ab78cf41
anup <anup@anups-MacBook-Air.local>
2016-02-04 23:17:45 +0530

    firstcommit

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,12 +3,12 @@ class Ability
 
   def initialize(user)
 
-   if user.admin?
-      can :manage, :all
-    else
-      can :read, :all
-    end
-    # Define abilities for the passed in user here. For example:
+   # if user.admin?
+   #    can :manage, :all
+   #  else
+   #    can :read, :all
+   #  end
+   #  # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
     #   if user.admin?
